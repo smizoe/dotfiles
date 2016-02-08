e9e305f3c9f86f2d7ac41d5c462a1ddc9ea153b5
Sho Mizoe <sho.mizoe@gmail.com>
2016-02-08 00:09:13 -0800

    avoid an empty argument

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -3,4 +3,4 @@
 if [ -f /etc/arch-release ] ; then
   OPTS="-e ansible_python_interpreter=/usr/bin/python2 -e virtualenv_executable=virtualenv2"
 fi
-ansible-playbook -i '127.0.0.1,' -c local setup.yml -K --ask-vault-pass "${OPTS}" "$@"
+ansible-playbook -i '127.0.0.1,' -c local setup.yml -K --ask-vault-pass ${OPTS} "$@"
