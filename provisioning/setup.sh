7087c4775ad0df57cdae9ddd9cd5288b28626341
Sho Mizoe <sho.mizoe@gmail.com>
2016-02-08 17:03:22 +0900

    added several configurations to run pip module on Archlinux

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ansible-playbook -i '127.0.0.1,' -c local setup.yml -K --ask-vault-pass "$@"
+
+if [ -f /etc/arch-release ] ; then
+  OPTS="-e ansible_python_interpreter=/usr/bin/python2 -e virtualenv_executable=virtualenv2"
+fi
+ansible-playbook -i '127.0.0.1,' -c local setup.yml -K --ask-vault-pass "${OPTS}" "$@"
