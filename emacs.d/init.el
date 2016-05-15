bd13fa6727d68d068ff225c61428a75e5d26002d
Sho Mizoe <sho.mizoe@gmail.com>
2016-05-14 21:39:21 -0700

    prepared for modularlizing settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,18 @@
+;; settings to modularize settings
+;; see http://stackoverflow.com/questions/2079095/how-to-modularize-an-emacs-configuration
+(defconst user-init-dir
+  (cond ((boundp 'user-emacs-directory)
+         user-emacs-directory)
+        ((boundp 'user-init-directory)
+         user-init-directory)
+        (t "~/.emacs.d/")))
+
+
+(defun load-user-file (file)
+  (interactive "f")
+  "Load a file in current user's configuration directory"
+  (load-file (expand-file-name file (concat user-init-dir "inits"))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; use emacs in server mode ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
