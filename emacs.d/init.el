13733df3a95a8579cb85f304ea160a6e90cb0a08
Sho Mizoe <sho.mizoe@gmail.com>
2016-05-14 21:32:54 -0700

    removed unnecessary comments

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,9 +13,6 @@
 
 ;; package manager
 (require 'package)
-;;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa-stable" . "http://stable.melpa.org/packages/")
