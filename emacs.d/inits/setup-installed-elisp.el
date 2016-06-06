e2db8f19d5ca72974ed45ce9fa864d4ac4bd6f16
Sho Mizoe <sho.mizoe@gmail.com>
2016-06-06 00:15:10 -0700

    specified cmigemo by full-path since cmigemo is not on PATH of emacs

--- emacs.d/inits/setup-installed-elisp.el
+++ emacs.d/inits/setup-installed-elisp.el
@@ -76,12 +76,14 @@
 ;; (package-install 'migemo)
 ;;; you must install cmigemo independently of this elisp.
 (require 'migemo)
-(setq migemo-command "cmigemo")
+(setq migemo-command "/usr/local/bin/cmigemo")
 (setq migemo-options '("-q" "--emacs" "-i" "\a"))
 (setq migemo-dictionary "/usr/local/Cellar/cmigemo/20110227/share/migemo/utf-8/migemo-dict")
 (setq migemo-coding-system 'utf-8-unix)
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
+(migemo-init)
+
 ;; python and venv
 ;; (package-install 'pyvenv)
 (setq
