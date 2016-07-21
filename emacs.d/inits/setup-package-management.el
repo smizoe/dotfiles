48dccaf1350fa967c48ab79261ee33bf63cf6c50
Sho Mizoe <sho.mizoe@gmail.com>
2016-07-21 15:17:38 -0700

    removed unncessary load-path

--- emacs.d/inits/setup-package-management.el
+++ emacs.d/inits/setup-package-management.el
@@ -50,7 +50,6 @@
 
 ;; auto-install.el
 ;; (package-install 'auto-install)
-(add-to-list 'load-path "~/.emacs.d/elisp/auto-install/")
 ;; add to load-path the directory where elisps installed by auto-install reside
 (add-to-list 'load-path "~/.emacs.d/auto-install/")
 (require 'auto-install)
