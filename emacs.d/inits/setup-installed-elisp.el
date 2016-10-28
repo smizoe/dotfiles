9101bfd4884ba4b4dda26b1c21bc0e8ea421fd7a
Sho Mizoe <sho.mizoe@gmail.com>
2016-10-27 22:51:55 -0700

    set shortcuts for helm-projectile-related functions

--- emacs.d/inits/setup-installed-elisp.el
+++ emacs.d/inits/setup-installed-elisp.el
@@ -193,8 +193,19 @@
 (use-package helm-projectile
   :ensure t
   :config
-  (with-eval-after-load 'projectile
-    (helm-projectile-on)
+  (progn
+    (with-eval-after-load 'projectile
+      (helm-projectile-on)
+      )
+    (with-eval-after-load 'evil-leader
+      (evil-leader/set-key
+        "ps" 'helm-projectile-switch-project
+        "pe" 'helm-projectile-find-file
+        "pf" 'helm-projectile-find-file-in-known-projects
+        "pxf" 'helm-projectile-recentf
+        "pd" 'helm-projectile-find-dir
+        )
+      )
     )
   :pin melpa-stable
   )
