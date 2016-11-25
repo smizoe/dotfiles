3d7a93e8a55f4241964fa275ddfe4e8638f554f2
Sho Mizoe <sho.mizoe@gmail.com>
2016-11-24 23:02:13 -0800

    remove goto-chg since the same functionality is available in evil with 'g;' and 'g,'

--- emacs.d/inits/setup-installed-elisp.el
+++ emacs.d/inits/setup-installed-elisp.el
@@ -87,21 +87,6 @@
     )
   )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; goto-chg.el (move to recently edited position) ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (install-elisp-from-emacswiki "goto-chg.el")
-(use-package goto-chg
-  :ensure t
-  :config
-  (progn
-    (define-key global-map (kbd "<f8>") 'goto-last-change)
-    ;;(define-key global-map (kbd "S-<f8>") 'goto-last-change-reverse)
-    (global-set-key "\M-[29~" 'goto-last-change-reverse)
-    )
-  )
-
 ;;;;;;;;;;;;;
 ;; helm-ag ;;
 ;;;;;;;;;;;;;
