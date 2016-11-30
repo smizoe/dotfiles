a5c2337f51ae8abb2a9facc8c87c3815e518565d
Sho Mizoe <sho.mizoe@gmail.com>
2016-11-30 08:57:25 -0800

    cd into the script directory before execution

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -3,4 +3,7 @@
 if [ -f /etc/arch-release ] ; then
   OPTS="-e ansible_python_interpreter=/usr/bin/python2 -e virtualenv_executable=virtualenv2"
 fi
+
+DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
+cd "${DIR}"
 ansible-playbook -i 'localhost,' -c local setup.yml -K --ask-vault-pass ${OPTS} "$@"
