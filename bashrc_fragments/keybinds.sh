0dab2525191c61e0cef65a8135033b85bf1e59bf
Sho Mizoe <sho.mizoe@gmail.com>
2017-03-07 22:10:52 -0800

    added helm-ag from bash

--- bashrc_fragments/keybinds.sh
+++ bashrc_fragments/keybinds.sh
@@ -1,3 +1,4 @@
 bind -x '"\C-r": peco_select_history "${READLINE_LINE}"'
 bind -x '"\er": source ~/.bashrc'
-bind -x '"\C-s": em -nw -e '"'"'(progn (setq-local helm-dash-docsets (helm-dash-installed-docsets))(helm-dash))'"'"''
+bind -x '"\eh": em -nw -e '"'"'(progn (setq-local helm-dash-docsets (helm-dash-installed-docsets))(helm-dash))'"'"''
+bind -x '"\C-s": em -nw -e "(helm-ag (expand-file-name \"${READLINE_LINE}\"))"'
