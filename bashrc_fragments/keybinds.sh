b8fe28b0779ecd909b6c89916b5e98c4c0b4d29e
Sho Mizoe <sho.mizoe@gmail.com>
2017-07-17 15:09:36 -0700

    removed unused keybinds and added a new one

--- bashrc_fragments/keybinds.sh
+++ bashrc_fragments/keybinds.sh
@@ -1,4 +1,3 @@
 bind -x '"\C-r": peco_select_history "${READLINE_LINE}"'
 bind -x '"\er": source ~/.bashrc'
-bind -x '"\eh": em -nw -e '"'"'(progn (setq-local helm-dash-docsets (helm-dash-installed-docsets))(helm-dash))'"'"''
-bind -x '"\C-s": em -nw -e "(helm-ag (expand-file-name \"${READLINE_LINE}\"))"'
+bind -x '"\C-s": elinks http://localhost:54321/'
