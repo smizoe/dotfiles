42eb990a033d5456e3a933d7942c6f4a8790b0bf
Sho Mizoe <sho.mizoe@gmail.com>
2017-08-27 15:00:57 -0700

    added ltsv2json function

--- bashrc_fragments/functions.sh
+++ bashrc_fragments/functions.sh
@@ -205,4 +205,8 @@ CMD:${last_cmd}" | tr "\n" "\t" >> "$1"
     echo >> "$1"
 }
 
+function ltsv2json() {
+  cat "${@:-/dev/stdin}"| ruby -rjson -nlpe '$_ = JSON.generate($_.split("\t").map{|elem| elem.split(":",2) }.inject({}){|acc, pair| acc[pair[0]] = pair[1]; acc})'
+}
+
 export -f peco_glimpse_fn kill_pane_from_pane_file print_tmux_info
