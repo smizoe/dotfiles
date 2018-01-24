3b5edf4c645bb93dada262c83c8f946fe865c95c
Sho Mizoe <sho.mizoe@gmail.com>
2018-01-23 20:00:36 -0800

    changed org-babel-python-command

--- emacs.d/inits/setup-installed-elisp.el
+++ emacs.d/inits/setup-installed-elisp.el
@@ -293,6 +293,7 @@
   :config
   (progn
     (setq
+     org-babel-python-command "ipython --simple-prompt -i"
      python-shell-interpreter "ipython"
      python-shell-interpreter-args "--simple-prompt -i"
      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
