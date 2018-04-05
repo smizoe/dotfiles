b21eacd414a043964935f548549f00fbebfbadc2
Sho Mizoe <sho.mizoe@gmail.com>
2018-04-05 15:54:16 -0700

    bind only when it is ouside of emacs

--- bashrc_fragments/keybinds.sh
+++ bashrc_fragments/keybinds.sh
@@ -1,3 +1,5 @@
-bind -x '"\C-r": peco_select_history "${READLINE_LINE}"'
-bind -x '"\er": source ~/.bashrc'
-bind -x '"\C-s": elinks http://localhost:54321/'
+if [ -z "${INSIDE_EMACS}" ] ; then
+    bind -x '"\C-r": peco_select_history "${READLINE_LINE}"'
+    bind -x '"\er": source ~/.bashrc'
+    bind -x '"\C-s": elinks http://localhost:54321/'
+fi
