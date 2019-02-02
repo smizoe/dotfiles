2e95c9a5964748168e593415de0e03900641bc1f
Sho Mizoe <sho.mizoe@gmail.com>
2019-02-01 21:24:56 -0800

    removed the trailing newlines when getting clipboard

--- local/bin/paste.sh
+++ local/bin/paste.sh
@@ -4,7 +4,7 @@ set -eu
 if which pbpaste &> /dev/null ; then
     pbpaste
 elif which powershell.exe &> /dev/null ; then
-    powershell.exe -Command "Get-Clipboard"
+    powershell.exe -Command "Get-Clipboard" | tr -d '\r\n'
 else
     xsel --clipboard --output
 fi
