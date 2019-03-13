646d45c0572b3a2a591ea720f1453ebbe121e30b
Sho Mizoe <sho.mizoe@gmail.com>
2019-03-12 17:30:33 -0700

    use dos2unix to handle newlines correctly

--- local/bin/paste.sh
+++ local/bin/paste.sh
@@ -4,7 +4,7 @@ set -eu
 if which pbpaste &> /dev/null ; then
     pbpaste
 elif which powershell.exe &> /dev/null ; then
-    powershell.exe -Command "Get-Clipboard" | tr -d '\r\n'
+    powershell.exe -Command "Get-Clipboard" | dos2unix.exe
 else
     xsel --clipboard --output
 fi
