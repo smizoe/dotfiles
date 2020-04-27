33788d24f4f39af30e3dcb96f6b35de13ddcffaa
Sho Mizoe <sho.mizoe@gmail.com>
2020-04-27 09:06:34 -0700

    installed yatex

---
+++ emacs.d/package-config/config-yatex.el
@@ -0,0 +1,39 @@
+;;; config-yatex.el ---                              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020
+
+(use-package yatex
+  :ensure t
+  :init
+  (progn
+    (setq auto-mode-alist (cons '("\\.tex$" . yatex-mode) auto-mode-alist))
+    (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+    )
+  :config
+  (progn
+    (with-eval-after-load 'evil
+      (progn
+                ;;;; yatex
+        (evil-define-key 'normal YaTeX-mode-map
+          "\\" (simulate-key-press YaTeX-prefix)
+          )
+        (evil-define-key 'visual YaTeX-mode-map
+          "\\" (simulate-key-press YaTeX-prefix)
+          )
+
+        ;; yahtml
+        (evil-define-key 'normal yahtml-mode-map
+          "\\" (simulate-key-press yahtml-prefix)
+          )
+        (evil-define-key 'visual yahtml-mode-map
+          "\\" (simulate-key-press yahtml-prefix)
+          )
+        )
+      )
+    )
+  )
+
+
+
+(provide 'config-yatex)
+;;; config-yatex.el ends here
