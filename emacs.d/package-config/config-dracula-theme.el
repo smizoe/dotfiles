e8f3c7a6c532b9e6296d149165aa05844dafa7fd
Sho Mizoe <sho.mizoe@gmail.com>
2020-05-29 21:07:36 -0700

    use dracula rather than zenburn

---
+++ emacs.d/package-config/config-dracula-theme.el
@@ -0,0 +1,15 @@
+;;; config-dracula-theme.el ---                      -*- lexical-binding: t; -*-
+
+;;; Code:
+(use-package dracula-theme
+  :ensure t
+  :pin melpa
+  :init
+  (progn
+    (load-theme 'dracula t)
+    )
+  )
+
+
+(provide 'config-dracula-theme)
+;;; config-dracula-theme.el ends here
