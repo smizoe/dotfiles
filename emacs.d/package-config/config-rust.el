7f5951595aa7ecb51ef0a031c847fd569ea08236
Sho Mizoe <sho.mizoe@gmail.com>
2020-06-01 21:10:43 -0700

    added keys for rust-specific commands

--- emacs.d/package-config/config-rust.el
+++ emacs.d/package-config/config-rust.el
@@ -2,8 +2,18 @@
 
 ;;; Code:
 (use-package rust-mode
-  :pin melpa-stable
+  :pin melpa
   :ensure t
+  :config
+  (with-eval-after-load 'evil-leader
+    (progn
+      (evil-leader/set-key-for-mode 'rust-mode
+        "rb" #'rust-compile
+        "rt" #'rust-test
+        "rr" #'rust-run
+        )
+      )
+    )
   )
 
 
