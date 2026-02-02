dd6199ae6e25cf0457cd22ea51c91445b7ef1a54
Sho Mizoe <sho.mizoe@gmail.com>
2026-02-01 22:08:29 -0800

    Clean up the configs

--- emacs.d/package-config/config-helm.el
+++ emacs.d/package-config/config-helm.el
@@ -3,7 +3,6 @@
 (use-package helm
   :ensure t
   :pin melpa-stable
-  :hook (emacs-startup . (lambda() (require 'helm-config)))
   :config
   (progn
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
@@ -11,6 +10,7 @@
     (define-key helm-map (kbd "C-z") 'helm-select-action)
     (define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
     (define-key isearch-mode-map (kbd "C-M-o") 'isearch-occur)
+    (helm-mode 1)
     )
   )
 
@@ -31,57 +31,6 @@
     )
   )
 
-(use-package helm-gtags
-  :ensure t
-  :init
-  (custom-set-variables
-    '(helm-gtags-suggested-key-mapping t)
-    )
-  )
-
-(defun gtags-root-dir ()
-  "Returns GTAGS root directory or nil if doesn't exist."
-  (with-temp-buffer
-    (if (zerop (call-process "global" nil t nil "-pr"))
-        (buffer-substring (point-min) (1- (point-max)))
-      nil)))
-
-(defun gtags-update ()
-  "Make GTAGS incremental update"
-  (interactive)
-  (start-process "global" nil "global" "-u" "--gtagslabel" "pygments"))
-
-(defun gtags-update-single(filename)
-  "Update Gtags database for changes in a single file"
-  (interactive)
-  (start-process "update-gtags" "update-gtags" "bash" "-c" (concat "cd " (gtags-root-dir) " ; gtags --gtagslabel pygments --single-update " filename )))
-
-(defun gtags-update-current-file()
-  (interactive)
-  (defvar filename)
-  (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
-  (gtags-update-single filename)
-  (message "Gtags updated for %s" filename))
-
-(defun gtags-update-hook()
-  "Update GTAGS file incrementally upon saving a file"
-  (when (gtags-root-dir)
-    (gtags-update-current-file)))
-
-(defun gtags-setup ()
-  (interactive)
-  (if (gtags-root-dir)
-      (message "gtags-root-dir exists: %s" (gtags-root-dir))
-    (let ((project-root (helm-ag--project-root)))
-      (if (y-or-n-p (format "Would you like to run gtags from %s?" project-root))
-          (let ((proc-name (format "setup-gtags: %s" project-root)))
-            (start-process proc-name proc-name "bash" "-c" (concat "cd " project-root "; gtags --gtagslabel pygments"))
-            )
-          )
-      )
-    )
-  )
-(add-hook 'after-save-hook #'gtags-update-hook)
 
 (use-package helm-projectile
   :ensure t
