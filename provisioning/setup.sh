7b1c706a581bcbcb5dd472084e3abec2dba72a35
Sho Mizoe <sho.mizoe@gmail.com>
2026-08-27 21:00:51 -0700

    Reduce the setup steps

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -2,4 +2,4 @@
 
 DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
 cd "${DIR}"
-ansible-playbook -i 'localhost,' -c local setup.yml -K --ask-vault-pass ${OPTS} "$@"
+ansible-playbook -i 'localhost,' -c local setup.yml -K ${OPTS} "$@"
